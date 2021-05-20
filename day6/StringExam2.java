8b082a8faa2ac183baef2e4bc8073ef15e5c054b
nororo29 <50298349+nororo29@users.noreply.github.com>
2021-05-20 12:51:26 +0900

    Add files via upload

---
+++ day6/StringExam2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringExam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//숫자 n 입력받아 1~n까지 소수 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			if (i % 2 == 0 || i % 3 == 0)
+				continue;
+			for (int j = 5; j <= i / j; j += 2) {
+				if (i % j == 0)
+					break;
+			}
+			System.out.print(i + " ");
+		}
+		
+		sc.close();
+	}
+
+}
