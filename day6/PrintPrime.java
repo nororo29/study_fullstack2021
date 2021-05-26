d0c714f9635d38a1b5268058aa9c21ae5616baf2
nororo29 <50298349+nororo29@users.noreply.github.com>
2021-05-26 11:01:03 +0900

    Update PrintPrime.java

--- day6/PrintPrime.java
+++ day6/PrintPrime.java
@@ -1,26 +1,48 @@
+package java_basic_day6;
+
 import java.util.Scanner;
 
 public class PrintPrime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//숫자 n 입력받아 1~n까지 소수 출력
-		
+		// 숫자 n 입력받아 1~n까지 소수 출력
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("숫자 입력 : ");
 		int n = sc.nextInt();
 		
-		for (int i = 0; i <= n; i++) {
-			if (i % 2 == 0 || i % 3 == 0)
-				continue;
-			for (int j = 5; j <= i / j; j += 2) {
-				if (i % j == 0)
-					break;
-			}
-			System.out.print(i + " ");
+		
+		if (n < 2) {
+			sc.close();
+			return;
+		}
+		if (n == 2) {
+			System.out.print(n);
+			sc.close();
+			return;
 		}
 		
+		System.out.print("2 3 ");
+		for (int i = 2; i <= n; i++) {
+			boolean isPrime = true;
+			
+			if (i % 2 == 0 || i % 3 == 0) {
+				isPrime = false;
+				continue;
+			}
+			
+			for (int j = 5; j <= i / j; j+=2) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime == true)
+				System.out.print(i + " ");
+		}
+		
+
 		sc.close();
 	}
 
